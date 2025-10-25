557271b7bd45fa88a5524c0a623fbdb0c14126ab
aieshatanveer <tanveer.aiesha786@gmail.com>
2025-10-25 10:20:21 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
-resource “local_file” “my_pet” {
+resource "local_file" "my_pet" {
    filename = var.filename
    content = var.content
 }
-resource “random_pet” “petname” {
-   prefix = “MR”
-   separator = “.”
-   length =”1”
+resource "random_pet" "petname" {
+   prefix = "MR"
+   separator = "."
+   length ="1"
 }
