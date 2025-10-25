f983d0a13756f02933798e0431b5955ea59e1579
aieshatanveer <tanveer.aiesha786@gmail.com>
2025-10-25 10:30:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,8 +1,8 @@
-Variable "filename" {
+variable "filename" {
     description = "the name of the file to create"
     type = string
 }
 variable "content" {
-    descripton = "the content to put inside the file"
+    description = "the content to put inside the file"
     type = string
 }
