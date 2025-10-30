3bd864ba3cb83f1f5533bf7f6c8299a2727e13bb
aieshatanveer <tanveer.aiesha786@gmail.com>
2025-10-30 14:54:38 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
-resource "local_file" "my_pet" {
-   filename = var.filename
-   content = var.content
+terraform{
+ required_providers{
+  local = {
+   source = "hashicorp/local"
+   version = "2.3.0"
+  }
+ }
 }
-resource "random_pet" "petname" {
-   prefix = "MR"
-   separator = "."
-   length ="1"
+variable "filename"{
+ type = list(string)
+ default = ["file1.txt","file2.txt", "file3.txt"]
+}
+
+resource "local_file" "pet"{
+ for_each = toset(var.filename)
+ filename = each.value
+ content = "Hello Terraform"
 }
