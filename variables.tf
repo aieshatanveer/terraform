fe7d9892bd8984f06c58eab799c67345a7645b8b
aieshatanveer <tanveer.aiesha786@gmail.com>
2025-10-30 15:01:23 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,10 +1,7 @@
-variable "filename" {
-    description = "the name of the file to create"
-    type = string
-    default = "example.txt"
-}
-variable "content" {
-    description = "the content to put inside the file"
-    type = string
-    default = "Hello from Jenkins!"
+variable "filename"{
+ default = [
+  "pets.txt",
+  "cats.txt",
+  "dogs.txt"
+ ]
 }
